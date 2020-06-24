b760c665e7013519f7bbabbd5851182e317df140
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-06-25 03:10:09 +0530

    added execute method in main interface

--- db.go
+++ db.go
@@ -1,12 +1,18 @@
 package db
 
+const (
+    DatabaseErrorRecordNotFound = "record_not_found"
+    DatabaseErrorUnknown        = "unknown_db_error"
+)
+
 type IDb interface {
-	InitDatabase() (err error)
-	Persist(queryString string, val ...interface{}) (e error)
-	FindOne(queryString string, mapper RowMapper, val ...interface{}) (result interface{}, e error)
-	FindAll(queryString string, mapper RowMapper, val ...interface{}) (result []interface{}, e error)
+    InitDatabase() (err error)
+    Persist(queryString string, val ...interface{}) (e error)
+    FindOne(queryString string, mapper RowMapper, val ...interface{}) (result interface{}, e error)
+    FindAll(queryString string, mapper RowMapper, val ...interface{}) (result []interface{}, e error)
+    Execute(queryString string, val ...interface{}) (result interface{}, e error)
 }
 
 type RowMapper interface {
-	Map(row interface{}) (result interface{}, err error)
+    Map(row interface{}) (result interface{}, err error)
 }
